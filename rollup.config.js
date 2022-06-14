0f231028316f2fa6ac096a5a64ed5272c3cc4dc0
Paul Mosquera <edisonpaul4@gmail.com>
2022-06-14 10:33:44 -0500

    Ready

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,29 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
+import babel from '@rollup/plugin-babel';
+import external from 'rollup-plugin-peer-deps-external';
+import pkg from './package.json';
 import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
-import json from '@rollup/plugin-json';
 
-const packageJson = require("./package.json");
 
-export default [
-  {
-    input: "src/index.ts",
+export default {
+  input: "src/index.ts",
     output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
-      },
+        { file: pkg.main, format: 'cjs', sourcemap: true, },
+        { file: pkg.module, format: 'esm', sourcemap: true, }
     ],
     plugins: [
-        json(),
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+        external(),
+        babel({
+            exclude: 'node_modules/**',
+            babelHelpers: 'bundled' 
+        }),
+        
+        typescript({ tsconfig: "./tsconfig.json" }) ,
+        
     ],
-  },
-  {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-  },
-];
+    external: Object.keys(pkg.peerDependencies || {}),
+    
+};
+
